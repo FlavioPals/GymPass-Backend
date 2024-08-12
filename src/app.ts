421d90a01ab9d6541a25f281ff006c964f237cde
FlavioPals <flaviopals@hotmail.com>
2024-08-12 10:35:17 -0300

    feat: create db connection, create post route

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
 import fastify from "fastify";
-import {PrismaClient} from '@prisma/client'
+import {z} from 'zod'
+import { primsa } from "./lib/prisma";
 
-const prisma = new PrismaClient()
-prisma.user.create({
-    data: {
-        name: 'John Doe',
-        email: 'j@j.com',
-        
-    }
-})
 
 export const app = fastify();
+
+
+
+app.post('/users', async (request, reply) =>{
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email:z.string().email(),
+        password: z.string().min(6),
+    })
+    const {name, email, password} = registerBodySchema.parse(request.body)
+
+    await primsa.user.create({
+        data:{
+            name,
+            email,
+          password_hash:password 
+               }
+    })
+    return reply.status(201).send()
+})
+
