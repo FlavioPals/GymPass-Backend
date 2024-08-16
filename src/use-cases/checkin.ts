5fb4fdf533477e768cd6da8903cc5d2232e29522
FlavioPals <flaviopals@hotmail.com>
2024-08-15 21:52:44 -0300

    feat: create test for check in

--- src/use-cases/checkin.ts
+++ src/use-cases/checkin.ts
@@ -11,10 +11,17 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase{
-
     constructor(private checkInsRepository: CheckInsRepository){}
 
-    async execute({userId, gymId}: CheckInUseCaseRequest):Promise<CheckInUseCaseResponse>{
+    async execute({
+        userId, gymId
+    }: CheckInUseCaseRequest):Promise<CheckInUseCaseResponse> {
+        const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+        if(checkInOnSameDate){
+            throw new Error()
+        }
+
         const checkIn = await this.checkInsRepository.create({
             gym_id: gymId,
             user_id: userId,
