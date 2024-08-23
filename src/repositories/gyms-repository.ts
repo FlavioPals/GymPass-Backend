55948ac6aa41f6537e3a7273fad65858a701c10a
FlavioPals <flaviopals@hotmail.com>
2024-08-22 21:06:16 -0300

    updating  interface GymRepository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,7 +1,13 @@
 import { Gym, Prisma } from "@prisma/client";
 
+export interface findManyNearbyParams{
+    latitude:number
+    longitude:number
+}
+
 export interface GymRepository {
     findById(id: string): Promise<Gym | null>
+    findManyNearBy(params:findManyNearbyParams): Promise<Gym[]>
     searchMany(query: string, page: number): Promise<Gym[]>
-    create(data:Prisma.GymCreateInput): Promise<Gym>
+    create(data:Prisma.GymCreateInput): Promise<Gym>  
 }
