287b21b4b2b7ce31680330fda6adf7b4f41da413
FlavioPals <flaviopals@hotmail.com>
2024-08-24 11:20:46 -0300

    created the validate to the check-in, that it can not be able to check-in after 20 minutes of his creation

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,9 +1,9 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { CheckInsRepository } from "../repositories/check-ins-repository";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 import { InMemoryCheckInsRepository } from "../repositories/in-memory-respository/in-memory-check-ins-repository";
-import exp from "constants";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 let checkInsRepository:CheckInsRepository
 let sut:ValidateCheckInUseCase
@@ -12,6 +12,11 @@ describe('Validate Check In Use Case', () => {
     beforeEach(async () => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new ValidateCheckInUseCase(checkInsRepository)
+        vi.useFakeTimers()
+    })
+
+    afterEach(async () => {
+        vi.useRealTimers()
     })
 
     it('should be able to validate the check-in', async () => {
@@ -33,5 +38,20 @@ describe('Validate Check In Use Case', () => {
                 checkInId: 'inexistent-check-in-id',
             })
         ).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+    it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+        vi.setSystemTime(new Date(2023, 0, 1, 13, 40)) //utc
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id:'gym-01',
+            user_id:'user-01',
+        })
+        const twentyOneMinutesInMs = 1000 * 60 * 21
+        vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+        await expect(() => sut.execute({
+            checkInId:createdCheckIn.id,
+        })
+        ).rejects.toBeInstanceOf(LateCheckInValidationError)
     })
 })
--- src/utils/get-distance-between-coordinates.ts
+++ src/utils/get-distance-between-coordinates.ts
@@ -32,3 +32,4 @@ export function getDistanceBetweenCoordinates(
     
       return dist
 }
+
